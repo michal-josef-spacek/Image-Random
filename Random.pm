098712082039fe51b8a5f562430ba33492449599
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-02-28 18:09:48 +0100

    Prepare for 0.09 version

--- Random.pm
+++ Random.pm
@@ -10,7 +10,7 @@ use Imager;
 use Imager::Color;
 use List::MoreUtils qw(none);
 
-our $VERSION = 0.08;
+our $VERSION = 0.09;
 
 # Constructor.
 sub new {
@@ -298,6 +298,6 @@ L<http://skim.cz>
 
 =head1 VERSION
 
-0.08
+0.09
 
 =cut
