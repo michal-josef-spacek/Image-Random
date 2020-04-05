e35917578e1a59b05de743dc3b7ce27df3c4c44d
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-04-05 13:19:51 +0200

    Improve LICENSE AND COPYRIGHT section in doc

--- Random.pm
+++ Random.pm
@@ -305,8 +305,9 @@ L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
- © 2013-2020 Michal Josef Špaček
- BSD 2-Clause License
+© 2013-2020 Michal Josef Špaček
+
+BSD 2-Clause License
 
 =head1 VERSION
 
